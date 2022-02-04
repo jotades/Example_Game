d9f1f94c0e31baf2f986fb9fea87fae83c11e2da
jotades <giovanni.tauro.ux@gmail.com>
2022-02-04 19:57:08 +0100

    First_Example_GitHub

---
+++ PropertyTest_04_02_22/Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Property myProperty;
+    public GameObject Cube;
+    private Vector3 cubeRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            myProperty.Score += 1;
+            this.transform.Rotate(0, transform.rotation.y+myProperty.Score,0);
+            Debug.Log(myProperty.Score);
+            
+
+        }
+    }
+}
---
+++ PropertyTest_04_02_22/Assets/Script/Property.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Property : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int _score;
+    public Text scoreText;
+    private int _velocity;
+    public Text velocityText;
+    public int Score
+    {
+        get 
+        {
+            return _score;
+        }
+        set
+        {
+            _score = value;
+            scoreText.text = _score.ToString();            
+        }
+    }
+    public int Velocity
+    {
+        get
+        { 
+            return _velocity; 
+        }
+        set
+        {
+            _velocity = value;
+            velocityText.text = _velocity.ToString();
+        }
+    }
+}
