217d9ac294105336181d2174cafda4054636c5a8
jotades <giovanni.tauro.ux@gmail.com>
2022-02-04 21:59:33 +0100

    Update 4

--- PropertyTest_04_02_22/Assets/Script/Player.cs
+++ PropertyTest_04_02_22/Assets/Script/Player.cs
@@ -7,20 +7,22 @@ public class Player : MonoBehaviour
     public Property myProperty;
     public GameObject Cube;
     int i = 0;
+    private const float secondsToDegrees = 0.1f;
+    public Transform seconds;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cube.GetComponent<Transform>();
     }
     // Update is called once per frame
     void Update()
     {
-         
+        //automaticRotation();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             i++;
             myProperty.Score += 1;
-            this.transform.Rotate(0, transform.rotation.y + myProperty.Score, 0);
+            // this.transform.Rotate(0, transform.rotation.y + myProperty.Score, 0);
             Debug.Log(myProperty.Score);
             var cubeRenderer = Cube.GetComponent<Renderer>();
             if (i % 2 == 0)
@@ -28,12 +30,18 @@ public class Player : MonoBehaviour
                 cubeRenderer.material.SetColor("_Color", Color.red);
             }
             else
-            { 
-            cubeRenderer.material.SetColor("_Color", Color.green);
+            {
+                cubeRenderer.material.SetColor("_Color", Color.green);
             }
 
         }
 
-        }
     }
+    //public void automaticRotation()
+    //{
+
+    //    seconds.Rotate(Quaternion.Euler(0, 0, Time.deltaTime * secondsToDegrees).eulerAngles*100);
+
+    //}
+}
 
